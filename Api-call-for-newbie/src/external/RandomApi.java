38b9700077fa8cb23aeb9d99f8f07184fade69a9
JonTheJi <johnjiangtw0804@gmail.com>
2018-10-01 20:18:50 -0700

    init again. Decide what newbie api we are gonna use

--- Api-call-for-newbie/src/external/RandomApi.java
+++ Api-call-for-newbie/src/external/RandomApi.java
@@ -1,9 +1,10 @@
+package external;
 
 public class RandomApi {
 
 	public static void main(String[] args) {
 
 		System.out.println("Hello newbie");
-		System.out.println("Lets have our first Git commit!");
+		System.out.println("Lets have our first api call. This api is called GitHub Jobs");
 	}
 }
